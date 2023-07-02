22320fc7c2735d6ff2995b4f6c1caf0467bc201e
Ruslan Mikhalev <ruslan.mihalev@gmail.com>
2023-07-02 22:35:48 +1200

    Fixed request.log file name typo

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -3,7 +3,7 @@ const expressWinston = require('express-winston');
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log ' }),
+    new winston.transports.File({ filename: 'request.log' }),
   ],
   format: winston.format.json(),
 });

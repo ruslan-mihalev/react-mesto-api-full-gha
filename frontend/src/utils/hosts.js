bcaa43f788b5763e7fe96e8b41a6a8e58a561bea
Ruslan Mikhalev <ruslan.mihalev@gmail.com>
2023-07-10 15:21:28 +1200

    Change backend api to HTTPS version

--- frontend/src/utils/hosts.js
+++ frontend/src/utils/hosts.js
@@ -1,2 +1,2 @@
-export const BASE_AUTH_URL = 'http://api.travelplaces.api.nomoreparties.sbs';
-export const BASE_API_URL = 'http://api.travelplaces.api.nomoreparties.sbs';
+export const BASE_AUTH_URL = 'https://api.travelplaces.api.nomoreparties.sbs';
+export const BASE_API_URL = 'https://api.travelplaces.api.nomoreparties.sbs';
